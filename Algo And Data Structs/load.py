41b821029c7aa7659ca2b9cea5a0de195fa91a74
SeekTr <SiakaHassanTraore@gmail.com>
2023-05-26 22:56:42 -0400

    Reworked the comments a little.

--- Algo And Data Structs/load.py
+++ Algo And Data Structs/load.py
@@ -1,5 +1,14 @@
+"""
+This file is made to be imported into other files in the same directory so the sorting algortithms can be properly 
+tested properly.
+"""
 
-def openf(filename): #Loads the contents in a file into a list with each element representing a line of text
+
+
+def openf(filename): 
+    """
+    Loads the contents in a file into a list with each element representing a line of text
+    """
     ans = []
     with open(filename,"r") as f:
         for line in f:
