3c7fc5eeba994b206c27f63b4bda0e123d6a5b59
SeekTr <SiakaHassanTraore@gmail.com>
2023-05-26 22:18:21 -0400

    Slight changes

--- TKinter/tempConverter.py
+++ TKinter/tempConverter.py
@@ -13,16 +13,16 @@ class TempConverter:
         self.label = tk.Label(self.wind, text="Enter the temperature", font=('Arial', 16))
         self.label.pack(padx=10, pady=10)
         
-        self.ent = tk.Entry(self.wind, font=("Arial",16))
+        self.ent = tk.Entry(self.wind, font=("Arial",16)) # User enters temperature here
         self.ent.bind("<KeyPress>", self.convert)
         self.ent.pack()
 
-        self.btn = tk.Button(self.wind, text="Convert", font=("Arial",18), command=self.convert)
-        self.btn.pack()
+        self.btn = tk.Button(self.wind, text="Convert", font=("Arial",18), command=self.convert) #Button for conversion
+        self.btn.pack() 
 
         self.wind.mainloop()
 
-    def convert(self, event=None): 
+    def convert(self, event=None): #Need to add "Enter" as a shortcut
         """ 
         The plan is to have the function take two inputs, the 'number' that needs to be converted and the 'type' it is to 
         be converted to. It will only accept "Fahrenheit, Celsius and Kelvin" as valid arguements for the tpye parameter
