21ea3fb8958e5c4befce1a89388b176d1cabc7eb
SeekTr <SiakaHassanTraore@gmail.com>
2023-06-07 23:13:28 -0400

    Comments

--- Data Science/panda.py
+++ Data Science/panda.py
@@ -1,5 +1,15 @@
 import pandas as pd
 
-x={'Student':['David','Samuel','Terry','Evan'],'Age':[27,24,22,32],'County':['UK','Canada','China','USA'],'Course':['Python','Data Structures','Machine Learning','Web Development'],'Marks':[85,72,89,76]}
+
+# Create a dictionary with the data
+x = {'Student': ['David', 'Samuel', 'Terry', 'Evan'],
+     'Age': [27, 24, 22, 32],
+     'County': ['UK', 'Canada', 'China', 'USA'],
+     'Course': ['Python', 'Data Structures', 'Machine Learning', 'Web Development'],
+     'Marks': [85, 72, 89, 76]}
+
+# Create a DataFrame using the dictionary
 df = pd.DataFrame(x)
+
+# Print the type of the DataFrame subset containing the 'Student' column
 print(type(df[['Student']]))
